c8342f9949307822e0182d64bc6361339d712ff0
okcowboy80 <okcowboy80@gmail.com>
2022-11-07 22:30:31 -0600

    removed files and added about me content

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-import { myConfig, myKey } from './myConfig.js';
-let val = myKey(); 
 
-// rest API to Use:
-/* 
-
-https://api.maas2.apollorion.com/
-
-{
-  "status": 200,
-  "id": 3251,
-  "terrestrial_date": "2022-03-24T00:00:00.000Z",
-  "ls": 195,
-  "season": "Month 7",
-  "min_temp": -68,
-  "max_temp": -5,
-  "pressure": 760,
-  "pressure_string": "Higher",
-  "abs_humidity": null,
-  "wind_speed": null,
-  "atmo_opacity": "Sunny",
-  "sunrise": "05:18",
-  "sunset": "17:22",
-  "local_uv_irradiance_index": "Moderate",
-  "min_gts_temp": -66,
-  "max_gts_temp": 5,
-  "wind_direction": null,
-  "sol": 3423,
-  "unitOfMeasure": "Celsius",
-  "TZ_Data": "America/Port_of_Spain"
+function randomizeFacts(response) {
+  let article = Math.floor(Math.random() * response.length);
+  while (response[article].details.length > 300) {
+    article = Math.floor(Math.random() * response.length);
+  }
+  return article;
 }
 
-*/
+fetch('https://api.spacexdata.com/v4/history')
+	.then(response => response.json())
+	.then(response => {
 
+    const article = randomizeFacts(response);
+    
+    let info = "";
+    let title = response[article].title;
+    let details = response[article].details;
+
+    info = `<h3>${title}</h3>`;
+    info += `<h4>${details}</h4>`
+    document.getElementById("speech-paragraph").innerHTML = info;
+    //console.log(response)
+  })
+	.catch(err => console.error(err));
