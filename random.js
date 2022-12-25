8f736ca6243b89c79f484b4c98a86e4a1f44807f
okcowboy80 <okcowboy80@gmail.com>
2022-12-25 00:41:31 -0600

    Randomized comet size

--- random.js
+++ random.js
@@ -1,8 +1,10 @@
 function myCallback() {
   let random = Math.floor(Math.random() * 400);
+  let scale = Math.floor(Math.random() * 100);
   //const comet = document.getElementById("comet");
   const root = document.documentElement;
   root.style.setProperty("--change", random + "px");
+  root.style.setProperty("--scale", scale + "px");
   //comet.style.top = "" + random + "px;";
   //console.log("Random is: " + random + "px;");
 }
